721be05040f3877337eb6a00d199463712a2cbc3
Jaser Akuly <jaser.akuly@gmail.com>
2016-11-27 19:21:39 -0600

    added profile
    added route and declaration

---
+++ app/components/profile.components.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'profile',
+    templateUrl: `profile.components.html`,
+})
+export class ProfileComponent { }
